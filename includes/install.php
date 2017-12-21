5a3bfb340a74b1fe85d003b2863ba98303619413
WP <34419856+wp-phpunit-bot@users.noreply.github.com>
2017-12-21 17:09:37 +0000

    Building 4.1.11

--- includes/install.php
+++ includes/install.php
@@ -22,6 +22,11 @@ require_once ABSPATH . '/wp-settings.php';
 require_once ABSPATH . '/wp-admin/includes/upgrade.php';
 require_once ABSPATH . '/wp-includes/wp-db.php';
 
+// Override the PHPMailer
+global $phpmailer;
+require_once( dirname( __FILE__ ) . '/mock-mailer.php' );
+$phpmailer = new MockPHPMailer();
+
 $wpdb->query( 'SET storage_engine = INNODB' );
 $wpdb->select( DB_NAME, $wpdb->dbh );
 
@@ -51,4 +56,5 @@ if ( $multisite ) {
 
 	install_network();
 	populate_network( 1, WP_TESTS_DOMAIN, WP_TESTS_EMAIL, $title, '/', $subdomain_install );
+	$wp_rewrite->set_permalink_structure( '' );
 }
