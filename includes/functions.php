8756df385b90527dbc87003eb64ed360d45b54bf
WP <34419856+wp-phpunit-bot@users.noreply.github.com>
2017-12-21 17:10:12 +0000

    Building 4.6.1

--- includes/functions.php
+++ includes/functions.php
@@ -1,5 +1,21 @@
 <?php
 
+/**
+ * Resets various `$_SERVER` variables that can get altered during tests.
+ */
+function tests_reset__SERVER() {
+	$_SERVER['HTTP_HOST']       = WP_TESTS_DOMAIN;
+	$_SERVER['REMOTE_ADDR']     = '127.0.0.1';
+	$_SERVER['REQUEST_METHOD']  = 'GET';
+	$_SERVER['REQUEST_URI']     = '';
+	$_SERVER['SERVER_NAME']     = WP_TESTS_DOMAIN;
+	$_SERVER['SERVER_PORT']     = '80';
+	$_SERVER['SERVER_PROTOCOL'] = 'HTTP/1.1';
+
+	unset( $_SERVER['HTTP_REFERER'] );
+	unset( $_SERVER['HTTPS'] );
+}
+
 // For adding hooks before loading WP
 function tests_add_filter($tag, $function_to_add, $priority = 10, $accepted_args = 1) {
 	global $wp_filter, $merged_filters;
@@ -42,3 +58,99 @@ function _delete_all_posts() {
 	}
 }
 
+class Basic_Object {
+	private $foo = 'bar';
+
+	public function __get( $name ) {
+		return $this->$name;
+	}
+
+	public function __set( $name, $value ) {
+		return $this->$name = $value;
+	}
+
+	public function __isset( $name ) {
+		return isset( $this->$name );
+	}
+
+	public function __unset( $name ) {
+		unset( $this->$name );
+	}
+
+	public function __call( $name, $arguments ) {
+		return call_user_func_array( array( $this, $name ), $arguments );
+	}
+
+	private function callMe() {
+		return 'maybe';
+	}
+}
+
+class Basic_Subclass extends Basic_Object {}
+
+function _wp_die_handler( $message, $title = '', $args = array() ) {
+	if ( !$GLOBALS['_wp_die_disabled'] ) {
+		_wp_die_handler_txt( $message, $title, $args);
+	} else {
+		//Ignore at our peril
+	}
+}
+
+function _disable_wp_die() {
+	$GLOBALS['_wp_die_disabled'] = true;
+}
+
+function _enable_wp_die() {
+	$GLOBALS['_wp_die_disabled'] = false;
+}
+
+function _wp_die_handler_filter() {
+	return '_wp_die_handler';
+}
+
+function _wp_die_handler_txt( $message, $title, $args ) {
+	echo "\nwp_die called\n";
+	echo "Message : $message\n";
+	echo "Title : $title\n";
+	if ( ! empty( $args ) ) {
+		echo "Args: \n";
+		foreach( $args as $k => $v ){
+			echo "\t $k : $v\n";
+		}
+	}
+}
+
+/**
+ * Set a permalink structure.
+ *
+ * Hooked as a callback to the 'populate_options' action, we use this function to set a permalink structure during
+ * `wp_install()`, so that WP doesn't attempt to do a time-consuming remote request.
+ *
+ * @since 4.2.0
+ */
+function _set_default_permalink_structure_for_tests() {
+	update_option( 'permalink_structure', '/%year%/%monthnum%/%day%/%postname%/' );
+}
+
+/**
+ * Helper used with the `upload_dir` filter to remove the /year/month sub directories from the uploads path and URL.
+ */
+function _upload_dir_no_subdir( $uploads ) {
+	$subdir = $uploads['subdir'];
+
+	$uploads['subdir'] = '';
+	$uploads['path'] = str_replace( $subdir, '', $uploads['path'] );
+	$uploads['url'] = str_replace( $subdir, '', $uploads['url'] );
+
+	return $uploads;
+}
+
+/**
+ * Helper used with the `upload_dir` filter to set https upload URL.
+ */ 
+function _upload_dir_https( $uploads ) {
+	$uploads['url'] = str_replace( 'http://', 'https://', $uploads['url'] );
+	$uploads['baseurl'] = str_replace( 'http://', 'https://', $uploads['baseurl'] );
+
+	return $uploads;
+}
